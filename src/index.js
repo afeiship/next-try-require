c1c6630ee31bad562e18cb6a53afb58536734d85
afeiship <1290657123@qq.com>
2021-06-10 17:54:48 +0800

    fix: defs conflict

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@
   nx.tryRequire = function (inPath, inDefaults, inOptions) {
     var options = nx.mix(null, defaults, inOptions);
     var path = options.context + inPath;
-    var defaults = options.context + inDefaults;
+    var defs = options.context + inDefaults;
     var res;
     try {
       res = options.relative ? require('.' + path) : require('@' + path);
     } catch (e) {
-      res = options.relative ? require('.' + defaults) : require('@' + defaults);
+      res = options.relative ? require('.' + defs) : require('@' + defs);
     }
     return res;
   };
